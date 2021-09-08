cff194bd4fff129211dfee100ca80e28e3d0975f
bill.mark <2286449127@qq.com>
2021-09-08 14:56:37 +0800

    3344

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-     11112222
+     11112222 333333333344444444
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
